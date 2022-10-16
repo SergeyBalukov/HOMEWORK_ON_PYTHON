a2e013d7c15fe0b61b1ef1d88153698191a1f9e8
Sergey Balukov <bserghey@yandex.ru>
2022-10-17 00:18:26 +0300

    add lesson5

---
+++ Lesson5/Task1_Player_VS_Bot.py
@@ -0,0 +1,56 @@
+# вариант человек против бота:
+
+from itertools import count
+from random import randint
+from tkinter.tix import Tree
+
+def input_dat(name):
+    x = int(input(f"{name}, введите количество конфет, которое возьмете от 1 до 28:"))
+    while x < 1 or x > 28:
+        x = int(input(f"{name}, введите корректное количество конфет: "))
+    return x
+
+
+
+def p_print(name, k , counter, value):
+    print(f"Ходил {name}, он взял {k}, теперь у него {counter}. Осталовь на столе {value} конфет")
+
+player1 = input("Введите имя игрока: ")
+player2 = "Bot"
+value = int(input("введите колличество конфет на столе: "))
+flag = randint(0,2) # флаг очередности
+if flag:
+    print(f"Первый ходит {player1}")
+else:
+    print(f"Первый ходит {player2}")
+
+counter1 = 0
+counter2 = 0
+
+while value > 28:
+    if flag:
+        k = input_dat(player1)
+        counter1 += k
+        value -= k
+        flag = False
+        p_print(player1, k, counter1, value)
+    else:
+        k = randint(1, 29)
+        counter2 += k
+        value -=k
+        flag = True
+        p_print(player2, k, counter2, value)
+
+if flag:
+    print(f"Выиграл {player1}")
+else:
+    print(f"Выиграл {player2}")
+
+
+
+
+
+
+
+
+
